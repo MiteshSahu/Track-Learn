691e7f0891be13031d1ee0d431ad2a5374784e0d
miteshsahu <sahu.mitesh2001@gmail.com>
2026-06-14 12:39:23 +0530

    added the redirect url

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.TRACK_LEARN_SUPABASE = {
   url: "https://yxdkblcakthixypsqgbe.supabase.co",
   anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4ZGtibGNha3RoaXh5cHNxZ2JlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEzNjM3OTQsImV4cCI6MjA5NjkzOTc5NH0.Y4yEXwq5jMi2xQhBBezO8ZHGjcD6RvJoaa3AbdWEZBg",
-  redirectUrl: "http://localhost:3000/"
+  redirectUrl: "https://miteshsahu.github.io/Track-Learn/"
 };
